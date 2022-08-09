f4115ffaa2fb7a2677d04f28d3d8e50e74255782
Gabriel Victor <cl2003@fiap.com.br>
2022-08-08 23:03:17 -0300

    TextInput

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View, StyleSheet } from 'react-native' 
+import { Text, TextInput, View} from 'react-native' 
 import {styles} from './styles'
 export default function Home(){
 
@@ -7,6 +7,14 @@ export default function Home(){
       <View style={{flex:1, backgroundColor:"#000000"}}>
         <Text style={styles.Texto} key="1">React</Text>
         <Text style={styles.Texto}key="2">Native</Text>
+        <TextInput 
+          style={styles.input}
+          placeholder="Nome do Participante"
+          placeholderTextColor="#F0FF0F"
+          keyboardType='default'
+        />
+
+
       </View>
     </>
   )
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -4,5 +4,12 @@ export const styles = StyleSheet.create({
     Texto: {
       fontSize:15,
       color:"white"
+    },
+    input: {
+      height:40,
+      backgroundColor: '#1F1E25',
+      color: '#FFFFFF',
+      padding:10,
+      textAlign:"left"
     }
 });
